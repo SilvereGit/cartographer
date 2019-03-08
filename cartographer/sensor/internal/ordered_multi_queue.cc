0c794b3b264074a420069bf003e027348b0561c3
Simo Gröhn <simo.grohn@silvere.com>
2019-03-08 07:49:14 +0200

    disabled Queue waiting for data: -messages

--- cartographer/sensor/internal/ordered_multi_queue.cc
+++ cartographer/sensor/internal/ordered_multi_queue.cc
@@ -153,7 +153,7 @@ void OrderedMultiQueue::CannotMakeProgress(const QueueKey& queue_key) {
   for (auto& entry : queues_) {
     if (entry.second.queue.Size() > kMaxQueueSize) {
       if (strcmp(queue_key.sensor_id.c_str(), "fix")) {
-        LOG_EVERY_N(WARNING, 60) << "Queue waiting for data: " << queue_key;
+        //LOG_EVERY_N(WARNING, 60) << "Queue waiting for data: " << queue_key;
       }
       return;
     }
